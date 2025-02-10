b757ff5e238a683bd8ac4dfa4cebeb69a568e31e
RShnitser <romanshnitser@hotmail.com>
2025-02-09 21:03:39 -0500

    add solutions

--- problems/easy.ts
+++ problems/easy.ts
@@ -10,6 +10,23 @@
  *
  */
 
+function twoSum(nums : number[], target: number): number[]{
+    const sumMap = new Map<number, number>();
+    for(let i = 0; i < nums.length; i++){
+        const num = nums[i];
+        const num2 = target - num;
+        const j = sumMap.get(num2);
+        if(j !== undefined){
+            return [j, i];
+        }
+        sumMap.set(num, i);
+    }
+    return [0, 0];
+}
+
+console.log("---twoSum---");
+console.log(twoSum([2, 7, 11, 15], 5));
+
 /*
  * Problem: Reverse Words in a String
  *
@@ -20,6 +37,21 @@
  * Output: "blue is sky the"
  *
  */
+
+function reverseWords(input : string) : string{
+    const words = input.split(" ");
+    let i = 0;
+    let j = words.length - 1;
+    while(i < j){
+        [words[i], words[j]] = [words[j], words[i]];
+        i++;
+        j--;
+    }
+    return words.join(" ");
+}
+
+console.log("---reverseWords---")
+console.log(reverseWords("the sky is blue"));
 
 /*
  * Problem: Most Common Character
@@ -32,6 +64,26 @@
  *
  */
 
+function mostCommonCharacter(input : string): string{
+    const letterMap = new Map<string, number>();
+    for(const l of input){
+        const newVal = (letterMap.get(l) || 0) + 1;
+        letterMap.set(l, newVal);
+    }
+    let maxCount = 0;
+    let result = "";
+    for(const [k, v] of letterMap.entries()){
+        if(v > maxCount){
+            maxCount = v;
+            result = k;
+        }
+    }
+    return result;
+}
+
+console.log("---mostCommonCharacter---");
+console.log(mostCommonCharacter("banana"));
+
 /*
  * Problem: Find Duplicates
  *
@@ -43,6 +95,21 @@
  *
  */
 
+function findDuplicated(nums : number[]) : number[]{
+    const numSet = new Set();
+    const result : number[] = [];
+    for(const n of nums){
+        if(numSet.has(n)){
+            result.push(n);
+        }
+        numSet.add(n);
+    }
+    return result;
+}
+
+console.log("---findDuplicates---");
+console.log(findDuplicated([4,3,2,7,8,2,3,1]));
+
 /*
  * Problem: First Unique Character
  *
@@ -53,6 +120,31 @@
  * Output: 0
  *
  */
+
+
+
+function firstUniqueCharacter(input: string) : number{
+    const letterMap = new Map<string, number[]>();
+    for(let i = 0; i < input.length; i++){
+        const l = input[i];
+        const indexCount = letterMap.get(l);
+        if(indexCount !== undefined){
+            letterMap.set(l, [indexCount[0], indexCount[1] + 1])
+        }else{
+            letterMap.set(l, [i, 1]);
+        }
+    }
+  
+    for(const [index, count] of letterMap.values()){
+        if(count === 1){
+            return index;
+        }
+    }
+    return -1;
+}
+
+console.log("--uniqueCharacter--");
+console.log(firstUniqueCharacter("leetcode"));
 
 /*
  * Problem: Find All Duplicates in an Array
--- problems/medium.ts
+++ problems/medium.ts
@@ -9,6 +9,34 @@
  *
  */
 
+function mergeOverlappingIntervals(input : number[][]) : number[][]{
+
+    if(input.length <= 1){
+        return input;
+    }
+    const result : number[][] = [];
+    result.push(input[0]);
+    outer:
+    for(let i = 1; i < input.length; i++){
+        for(let j = 0; j < result.length; j++){
+            const [il, ir] = input[i];
+            const [jl, jr] = result[j];
+            if(il <= jr){
+                result[j][0] = jl;
+                result[j][1] = ir;
+                continue outer;
+            }else if(il >= jl && ir <= jr){
+                continue outer;
+            }
+        }
+        result.push(input[i]);
+    }
+    return result;
+}
+
+console.log("--mergeInterval---");
+console.log(mergeOverlappingIntervals([[1,3],[2,6],[8,10],[15,18]]));
+
 /*
  * Problem: Group Anagrams
  *
@@ -20,6 +48,26 @@
  *
  */
 
+function groupAnagrams(input : string[]): string[][]{
+    const anagramMap = new Map<string, string[]>();
+    
+    for(const word of input){
+        const anagram = word.split("").sort().join("");
+        const group = anagramMap.get(anagram);
+        if(group !== undefined){
+            group.push(word);
+            anagramMap.set(anagram, group);
+        }else{
+            anagramMap.set(anagram, [word]);
+        }
+    }
+
+    return Array.from(anagramMap.values());
+}
+
+console.log("--groupAnagram--");
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+
 /*
  * Problem: Longest Palindromic Substring
  *
@@ -30,6 +78,65 @@
  * Output: "bab" (or "aba")
  *
  */
+
+function getPalindrome(input : string, start : number): string{
+    let result = "";
+    
+    let l = start;
+    let r = start + 1;
+    let evenResult = "";
+    while(true){
+        if(l < 0 || r > input.length - 1){
+            break;
+        }
+        if(input[l] === input[r]){
+            evenResult = `${input[l]}${evenResult}${input[r]}`;
+            l--;
+            r++;
+        }else{
+            break;
+        }
+    }
+    result = evenResult;
+   
+    let oddResult = input[start];
+    l = start - 1;
+    r = start + 1;
+
+    while(true){
+        if(l < 0 || r > input.length - 1){
+            break;
+        }
+        if(input[l] === input[r]){
+            oddResult = `${input[l]}${oddResult}${input[r]}`;
+            l--;
+            r++;
+        }else{
+            break;
+        }
+    }
+    if(oddResult.length > evenResult.length){
+        result = oddResult;
+    }
+    if(result.length === 1){
+        return "";
+    }
+    return result;
+}
+
+function findLongestPalindrome(input: string):string{
+    let result = "";
+    for(let i = 0; i < input.length; i++){
+        const palindrome = getPalindrome(input, i);
+        if(palindrome.length > result.length){
+            result = palindrome;
+        }
+    }
+    return result;
+}
+
+console.log("--longestPalindrome---");
+console.log(findLongestPalindrome("babad"));
 
 /*
  * Problem: Flatten Nested Object
@@ -43,6 +150,25 @@
  * Hint: Use recursion + a helper function.
  */
 
+function flattenObject(input : any): any {
+    const result = {};
+
+    for(const [k, v] of Object.entries(input)){
+        if(typeof v === "object"){
+            const flat = flattenObject(v);
+            for(const [kf, vf] of Object.entries(flat)){
+                result[kf] = vf;
+            }
+        }else{
+            result[k] = v;
+        }
+    }
+    return result;
+}
+
+console.log("---flattenObject---")
+console.log(flattenObject({ a: { b: { c: 1 } }, d: 2 }));
+
 /*
  * Problem: Deep Object Comparison
  *
@@ -55,6 +181,44 @@
  * Hint: Use recursion and check nested properties.
  */
 
+function compareObject(a : any, b: any): boolean {
+    let result = true;
+    const entriesA = Object.entries(a);
+    const entriesB = Object.entries(b);
+    if(entriesA.length !== entriesB.length){
+        return false;
+    }
+
+    for(let i = 0; i < entriesA.length; i++){
+        const [ka, va] = entriesA[i];
+        const [kb, vb] = entriesB[i];
+
+        if(ka !== kb){
+            return false;
+        }
+
+        if(typeof va !== typeof vb){
+            return false;
+        }
+
+        if(typeof va === "object"){
+            if(!compareObject(va, vb)){
+                return false;
+            }
+           
+        }
+        else if(va !== vb){
+            return false;
+        }
+    }
+
+   
+    return result;
+}
+
+console.log("---compareObject---");
+console.log(compareObject({ a: { b: 1 } }, { a: { b: 1 } }));
+
 /*
  * Problem: Maximum Subarray Sum
  *
@@ -65,3 +229,24 @@
  * Output: 6  // Subarray: [4,-1,2,1]
  *
  */
+
+function maxSubarray(nums : number[]): number{
+    let largestSum = Number.NEGATIVE_INFINITY;
+  
+    for(let subLength = 0; subLength < nums.length; subLength++){
+        for(let start = 0;  start < nums.length; start++){
+            let currSum = 0;
+            for(let i = 0; i <= subLength && start + subLength < nums.length; i++){
+                currSum += nums[start + i];
+            }
+            if(currSum > largestSum){
+                largestSum = currSum;
+            }
+        }
+    }
+
+    return largestSum;
+}
+
+console.log("---maxSubArray---");
+console.log(maxSubarray([-2,1,-3,4,-1,2,1,-5,4]));
